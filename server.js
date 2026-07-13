a0fe9e83bcd37a47f3cdcd8f4fc8789ca5290735
Hiiankit <ankit.thrives@gmail.com>
2026-07-13 16:20:51 +0530

    changed the logs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "Express server is running now checking ci-cd" });
+  res.json({ message: "hello class, we are good to go" });
 });
 
 app.listen(port, () => {
